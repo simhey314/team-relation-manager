fefd16b8adc90165c3d53e3cfd2701e316744403
Simon Heyden <simon@family-heyden.net>
2018-08-21 17:38:16 +0200

    avoid null pointer exceptions for arg/ result object's type

--- src/main/java/com/heyden/teamrelationmanager/aspect/CommonLoggingAspect.java
+++ src/main/java/com/heyden/teamrelationmanager/aspect/CommonLoggingAspect.java
@@ -22,20 +22,22 @@ import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class CommonLoggingAspect {
 	
+	private static final String RESULT_IS_NULL = "result is null";
+	private static final String ARG_IS_NULL = "arg is null";
 	private static final Logger LOG = LogManager.getLogger(CommonLoggingAspect.class); 
 	
 	@Before("com.heyden.teamrelationmanager.aspect.PointcutExpression.controllerDaoService()")
 	public void loggingAppFlowBefore(JoinPoint joinPoint) {
 		LOG.info("@Before on method: {}", joinPoint.getSignature().toShortString());
 		for (Object arg : joinPoint.getArgs()) {
-			LOG.debug("@Before argument: Type [{}] Value [{}]", arg.getClass().getName(), arg);
+
+			LOG.debug("@Before argument: Type [{}] Value [{}]", getTypeName(arg, ARG_IS_NULL) , arg);
 		} 
 	}
 	
@@ -43,12 +45,20 @@ public class CommonLoggingAspect {
 					returning="result")
 	public void loggingAppFlowAfterReturning(JoinPoint joinPoint, Object result) {
 		LOG.debug("@AfterReturning on method: {}", joinPoint.getSignature().toShortString());
-		LOG.debug("@AfterReturning result: Type [{}] Value [{}]", result.getClass().getName(), result);
+		LOG.debug("@AfterReturning result: Type [{}] Value [{}]", getTypeName(result, RESULT_IS_NULL));
 	}
 	
 	@AfterThrowing(pointcut="com.heyden.teamrelationmanager.aspect.PointcutExpression.controllerPackage()",
 			       throwing="exception")
 	public void loggingExceptions(JoinPoint joinPoint, Exception exception) {
 		LOG.error("Exception is thrown on method: " + joinPoint.getSignature().toShortString() + "\n", exception);
+	}
+	
+	private String getTypeName(Object object, String defaultValue) {
+		String type = defaultValue;
+		if (object != null) {
+			type = object.getClass().getName();
+		}
+		return type;
 	}
 }
