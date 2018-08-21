84d69c1d96feb480503023de9dc339b4f0c7bbd0
Simon Heyden <simon@family-heyden.net>
2018-08-21 13:16:18 +0200

    add the apache licence file comment

--- src/main/java/com/heyden/teamrelationmanager/controller/TeamController.java
+++ src/main/java/com/heyden/teamrelationmanager/controller/TeamController.java
@@ -1,3 +1,18 @@
+/** Copyright 2018 Simon Heyden
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. 
+**/
+
 package com.heyden.teamrelationmanager.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,10 +44,10 @@ public class TeamController {
 		return "redirect:/team/list";
 	}
 	
-	@PostMapping("/detail")
+	@GetMapping("/detail")
 	public String detailTeam(@RequestParam("teamId") int teamId, Model model) {
 		model.addAttribute("team", teamService.getTeam(teamId));
-		return "redirect:/team/list";
+		return "team/detail";
 	}
 	
 	@GetMapping("/list")
@@ -41,5 +56,13 @@ public class TeamController {
 		model.addAttribute("teams", teamService.getTeams(Team.COLUMN_NAME));
 		
 		return "team/list";
+	}
+	
+	@GetMapping("/create")
+	public String createTeam(Model model) {
+		
+		model.addAttribute("team", new Team());
+		
+		return "team/detail";
 	}
 }
--- src/main/java/com/heyden/teamrelationmanager/entity/Team.java
+++ src/main/java/com/heyden/teamrelationmanager/entity/Team.java
@@ -1,3 +1,18 @@
+/** Copyright 2018 Simon Heyden
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. 
+**/
+
 package com.heyden.teamrelationmanager.entity;
 
 import java.util.ArrayList;
@@ -11,6 +26,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @Table(name="team")
@@ -24,6 +40,7 @@ public class Team {
 	private int id;
 	
 	@Column()
+	@NotBlank(message="string.required")
 	private String name;
 	
 	@OneToMany(mappedBy="team", cascade= {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
@@ -71,6 +88,40 @@ public class Team {
 		
 		employees.add(newTeammember);
 		newTeammember.setTeam(this);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Team other = (Team) obj;
+		if (id != other.id) {
+			return false;
+		}
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!name.equals(other.name)) {
+			return false;
+		}
+		return true;
 	}
 
 	@Override
