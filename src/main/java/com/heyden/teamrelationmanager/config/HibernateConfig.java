5404e2cc8d927871dcbe302bb80589e33a7b921d
Simon Heyden <simon@family-heyden.net>
2018-08-22 16:53:52 +0200

    separate the hibernate config to the web config

---
+++ src/main/java/com/heyden/teamrelationmanager/config/HibernateConfig.java
@@ -0,0 +1,98 @@
+package com.heyden.teamrelationmanager.config;
+
+import java.beans.PropertyVetoException;
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+@Configuration
+@EnableTransactionManagement
+@PropertySource({ "classpath:mysql.properties" })
+public class HibernateConfig {
+	
+	private static final Logger LOG = LogManager.getLogger(HibernateConfig.class);
+
+	@Autowired
+	private Environment environment;
+
+	@Bean
+	public DataSource dataSource() {
+
+		// create connection pool
+		ComboPooledDataSource dataSource = new ComboPooledDataSource();
+
+		// set the jdbc driver
+		try {
+			dataSource.setDriverClass(environment.getProperty("jdbc.driver"));
+		} catch (PropertyVetoException exc) {
+			throw new RuntimeException(exc);
+		}
+
+		// just to make sure we are reading the data
+		LOG.info("jdbc.url=" + environment.getProperty("jdbc.url"));
+		LOG.info("jdbc.user=" + environment.getProperty("jdbc.user"));
+
+		// set database connection props
+		dataSource.setJdbcUrl(environment.getProperty("jdbc.url"));
+		dataSource.setUser(environment.getProperty("jdbc.user"));
+		dataSource.setPassword(environment.getProperty("jdbc.password"));
+
+		// set connection pool props
+		dataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+		dataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
+		dataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+		dataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
+
+		return dataSource;
+	}
+
+	@Bean
+	public LocalSessionFactoryBean sessionFactory() {
+		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+
+		// set hibernate properties
+		Properties property = new Properties();
+		property.setProperty("hibernate.dialect", environment.getProperty("hibernate.dialect"));
+		property.setProperty("hibernate.show_sql", environment.getProperty("hibernate.show_sql"));
+
+		sessionFactory.setDataSource(dataSource());
+		sessionFactory.setPackagesToScan(environment.getProperty("hibernate.packagesToScan"));
+		sessionFactory.setHibernateProperties(property);
+
+		return sessionFactory;
+	}
+
+	@Bean
+	@Autowired
+	public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+		HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+		transactionManager.setSessionFactory(sessionFactory);
+		return transactionManager;
+	}
+
+	private int getIntProperty(String propertyName) {
+		String propertyValue = environment.getProperty(propertyName);
+		int result = 0;
+		try {
+			result = Integer.parseInt(propertyValue);
+		} catch (NumberFormatException exception) {
+			LOG.error("Can't parse property int:", exception);
+		}
+		return result;
+	}
+	
+}
--- src/main/java/com/heyden/teamrelationmanager/config/WebAppConfig.java
+++ src/main/java/com/heyden/teamrelationmanager/config/WebAppConfig.java
@@ -15,26 +15,16 @@ limitations under the License.
 
 package com.heyden.teamrelationmanager.config;
 
-import java.beans.PropertyVetoException;
-import java.util.Properties;
-
-import javax.sql.DataSource;
+import java.util.Locale;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.core.env.Environment;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.ViewResolver;
@@ -43,75 +33,13 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-import com.mchange.v2.c3p0.ComboPooledDataSource;
-
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.heyden.teamrelationmanager")
-@EnableTransactionManagement
-@PropertySource({ "classpath:mysql.properties" })
 @EnableAspectJAutoProxy
 public class WebAppConfig implements WebMvcConfigurer {
-
+	
 	private static final Logger LOG = LogManager.getLogger(WebAppConfig.class);
-
-	@Autowired
-	private Environment environment;
-
-	@Bean
-	public DataSource myDataSource() {
-
-		// create connection pool
-		ComboPooledDataSource dataSource = new ComboPooledDataSource();
-
-		// set the jdbc driver
-		try {
-			dataSource.setDriverClass(environment.getProperty("jdbc.driver"));
-		} catch (PropertyVetoException exc) {
-			throw new RuntimeException(exc);
-		}
-
-		// just to make sure we are reading the data
-		LOG.info("jdbc.url=" + environment.getProperty("jdbc.url"));
-		LOG.info("jdbc.user=" + environment.getProperty("jdbc.user"));
-
-		// set database connection props
-		dataSource.setJdbcUrl(environment.getProperty("jdbc.url"));
-		dataSource.setUser(environment.getProperty("jdbc.user"));
-		dataSource.setPassword(environment.getProperty("jdbc.password"));
-
-		// set connection pool props
-		dataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
-		dataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
-		dataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
-		dataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
-
-		return dataSource;
-	}
-
-	@Bean
-	public LocalSessionFactoryBean sessionFactory() {
-		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-
-		// set hibernate properties
-		Properties property = new Properties();
-		property.setProperty("hibernate.dialect", environment.getProperty("hibernate.dialect"));
-		property.setProperty("hibernate.show_sql", environment.getProperty("hibernate.show_sql"));
-
-		sessionFactory.setDataSource(myDataSource());
-		sessionFactory.setPackagesToScan(environment.getProperty("hibernate.packagesToScan"));
-		sessionFactory.setHibernateProperties(property);
-
-		return sessionFactory;
-	}
-
-	@Bean
-	@Autowired
-	public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
-		HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-		transactionManager.setSessionFactory(sessionFactory);
-		return transactionManager;
-	}
 
 	@Bean
 	public ViewResolver viewResolver() {
@@ -124,6 +52,8 @@ public class WebAppConfig implements WebMvcConfigurer {
 		ReloadableResourceBundleMessageSource bean = new ReloadableResourceBundleMessageSource();
 		bean.setBasename("classpath:messages");
 		bean.setDefaultEncoding("UTF-8");
+		String result = bean.getMessage("Pattern.employee.email", null, Locale.GERMANY);
+		LOG.debug("===========================>>>>> Pattern.employee.email: " + result);
 		return bean;
 	}
 
@@ -131,7 +61,8 @@ public class WebAppConfig implements WebMvcConfigurer {
 	public Validator getValidator() {
 		LocalValidatorFactoryBean validatorBean = new LocalValidatorFactoryBean();
 		validatorBean.setValidationMessageSource(validatorMessageSource());
-
+		LOG.debug("===========================>>>>> getValidator(): ");
+		
 		return validatorBean;
 	}
 
@@ -140,14 +71,4 @@ public class WebAppConfig implements WebMvcConfigurer {
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
 	}
 
-	private int getIntProperty(String propertyName) {
-		String propertyValue = environment.getProperty(propertyName);
-		int result = 0;
-		try {
-			result = Integer.parseInt(propertyValue);
-		} catch (NumberFormatException exception) {
-			LOG.error("Can't parse property int:", exception);
-		}
-		return result;
-	}
 }
