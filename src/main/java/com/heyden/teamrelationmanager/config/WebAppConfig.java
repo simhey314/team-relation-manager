98251e9cb296da6c4a7e5f3ac0339cfcc42f459c
Simon Heyden <simon@family-heyden.net>
2018-09-05 14:30:07 +0200

    initinalize thymeleaf template engine
    - add config
    - add some template html files

--- src/main/java/com/heyden/teamrelationmanager/config/WebAppConfig.java
+++ src/main/java/com/heyden/teamrelationmanager/config/WebAppConfig.java
@@ -13,40 +13,88 @@
 
 package com.heyden.teamrelationmanager.config;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.heyden.teamrelationmanager")
 @EnableAspectJAutoProxy
-public class WebAppConfig implements WebMvcConfigurer {
+public class WebAppConfig implements WebMvcConfigurer, ApplicationContextAware {
 
+	private ApplicationContext applicationContext;
+
+//	@Bean
+//	public ViewResolver viewResolver() {
+//		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver("/WEB-INF/view/", ".jsp");
+//		return viewResolver;
+//	}
+	
+    @Bean
+    public ThymeleafViewResolver viewResolver(){
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+        viewResolver.setCharacterEncoding(StandardCharsets.UTF_8.displayName());
+        return viewResolver;
+    }
+	
 	@Bean
-	public ViewResolver viewResolver() {
-		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver("/WEB-INF/view/", ".jsp");
-		return viewResolver;
+	public SpringResourceTemplateResolver templateResolver() {
+		SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+		
+		templateResolver.setApplicationContext(applicationContext);
+		templateResolver.setPrefix("/WEB-INF/view/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		
+		return templateResolver;
+	}
+	
+	@Bean
+	public SpringTemplateEngine templateEngine() {
+	    SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+	    
+	    templateEngine.setTemplateResolver(templateResolver());
+	    templateEngine.setEnableSpringELCompiler(true);
+	    templateEngine.setEnableSpringELCompiler(false);
+	    
+	    return templateEngine;
 	}
 
 	@Bean
 	public MessageSource messageSource() {
 		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		
 		messageSource.setBasename("messages");
-		messageSource.setDefaultEncoding("UTF-8");
+		messageSource.setDefaultEncoding(StandardCharsets.UTF_8.displayName());
+		
 		return messageSource;
 	}
 
 	@Override
 	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
 	}
 }
